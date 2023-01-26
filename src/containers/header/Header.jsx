5eb5bf88819908db64a174453c3602c0dd97086a
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2023-01-26 18:15:30 +0100

    added respective header styling

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
-import people from '../../assets/people.png';
-import ai from '../../assets/ai.png';
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
@@ -21,12 +21,12 @@ const Header = () => {
           <button type="button">Get Started</button>
         </div>
         <div className="gpt3__header-content__people">
-          <img src={people} alt="people"/>
+          <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai"/>
-        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
